74dffbb80c90fa2c369f97580314f398f4cf08b7
José Pedro Cunha do Amaral <129435099+JosePCAmaral@users.noreply.github.com>
2024-08-26 11:07:01 -0300

    ex multa

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 59
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+if(vel > 60){
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO!!!')
+}
+console.log('use sempre o cinto de seurança')
---
+++ aula011/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'brasil'
+console.log(`Voce mora em ${pais}`)
+if(pais == 'brasil'){
+    console.log('voce é brasileiro')
+}else{
+    console.log('voce é estrangeiro')
+}
