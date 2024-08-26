79c0c7d649d0679a7b83ecc373cbc43e84617809
José Pedro Cunha do Amaral <129435099+JosePCAmaral@users.noreply.github.com>
2024-08-26 14:21:15 -0300

    preparatório ex2

--- aula-pratica/script.js
+++ aula-pratica/script.js
@@ -6,13 +6,13 @@ function carregar(){
     var h = data.getHours()
     hr.innerHTML = `Agora são ${h}:00hr`
     if(h < 12){
-        img.src = 'pexels-gareth-davies-230510-910411.jpg'
+        img.src = 'imagens/pexels-gareth-davies-230510-910411.jpg'
         body.style.background = '#f5cf73'
     }else if(h >= 12 && h < 18){
-        img.src = 'pexels-george-desipris-858241.jpg'
+        img.src = 'imagens/pexels-george-desipris-858241.jpg'
         body.style.background = '#96501d'
     }else{
-        img.src = 'pexels-enricoperini-705425.jpg'
+        img.src = 'imagens/pexels-enricoperini-705425.jpg'
         body.style.background = '#2d2d45'
     }
 }
