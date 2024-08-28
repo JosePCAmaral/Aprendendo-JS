abaffced6dafc1c614bb70544c8a68fc57b6e898
José Pedro Cunha do Amaral <129435099+JosePCAmaral@users.noreply.github.com>
2024-08-28 08:59:48 -0300

    desafio concluido

--- aula-pratica/scripts/script5.js
+++ aula-pratica/scripts/script5.js
@@ -1,6 +1,7 @@
 var tab = document.getElementById('seltab')
 var res = document.getElementById('res')
 var v = []
+var res = document.getElementById('res')
 
 function adicionar(){
     var num = document.getElementById('inum')
@@ -16,5 +17,39 @@ function adicionar(){
             tab.appendChild(item)
             v.push(n)
         }
+    }
+    res.innerHTML = ''
+    num.value = ""
+    num.focus()
+}
+
+function analizar(){
+    if(v.length == 0){
+        window.alert(`Adicione valores para procegir!`)
+    }else{
+        var total = v.length
+        var menor = v[0]
+        var maior = v[0]
+        var soma = 0
+        var media = 0
+
+        for(let pos in v){
+            soma += v[pos]
+            if(maior > v[pos]){
+                maior = v[pos]
+            }
+            if(menor < v[pos]){
+                menor = v[pos]
+            }
+        }
+        media = soma / total
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>Vetor: ${v}</p>`
+        res.innerHTML += `<p>Sua lista tem ${total} elementos</p>`
+        res.innerHTML += `<p>O maior numero da lista é: ${maior} </p>`
+        res.innerHTML += `<p>O menor numero da lista é: ${menor} </p>`
+        res.innerHTML += `<p>A soma dos elementos é: ${soma} </p>`
+        res. innerHTML += `<p>A media do se vetor é: ${media} </p>`
     }
 }
